8cb6c4fc654cb3742d338ed2a0751adf193a9367
henrycheung1020 <146144664+henrycheung1020@users.noreply.github.com>
2023-10-22 23:24:29 +0000

    commit

--- app.rb
+++ app.rb
@@ -7,3 +7,32 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/square/new") do
+  erb(:square)
+end 
+
+get("/square/results") do 
+  erb(:square_results)
+end
+
+get("/square_root/new") do 
+  erb(:squareroot)
+end
+
+get("/square_root/results")do
+  erb(:squareroot_results)
+end
+
+get("/payment/new") do 
+  erb(:payment)
+end 
+
+get("/payment/results") do
+  erb(:payment_results)
+end
+
+get("/random/new") do 
+  "
+  "
+end
