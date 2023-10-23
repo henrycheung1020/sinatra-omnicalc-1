433e141154d88315ff66dfccb2e8427f2b0e78c9
henrycheung1020 <146144664+henrycheung1020@users.noreply.github.com>
2023-10-23 03:47:12 +0000

    commit

--- app.rb
+++ app.rb
@@ -13,6 +13,8 @@ get("/square/new") do
 end 
 
 get("/square/results") do 
+  @num = params.fetch("number").to_f
+  @results = @num ** 2
   erb(:square_results)
 end
 
@@ -21,6 +23,8 @@ get("/square_root/new") do
 end
 
 get("/square_root/results")do
+  @num = params.fetch("number").to_f
+  @results = @num ** 0.5
   erb(:squareroot_results)
 end
 
@@ -29,10 +33,21 @@ get("/payment/new") do
 end 
 
 get("/payment/results") do
+  @apr = params.fetch("APR").to_f 
+  monthly_rate = (@apr/100) / 12.0
+  @years = params.fetch("years").to_f
+  @principal = params.fetch("principal").to_f
+  @payment = (@principal * monthly_rate) / (1 - (1 + monthly_rate) ** (-@apr*12))
   erb(:payment_results)
 end
 
 get("/random/new") do 
-  "
-  "
+  erb(:random)
+end
+
+get("/random/results") do
+  @min = params.fetch("min").to_f
+  @max = params.fetch("max").to_f
+  @random = rand(@min..@max)
+  erb(:random_results)
 end
